70ab818a824eaa5a5586469be5bea5b9bc606959
vanvu0211 <vu34304@gmail.com>
2024-11-10 00:35:57 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { paths } from './src/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  server:{
+    port: 3000,
+    proxy:{
+      '/api':{
+        target: "https://shrimppond.runasp.net",
+        changeOrigin:true,
+        rewrite:(paths) => paths.replace(/^\/api/,''),
+      }
+    }
+  },
   plugins: [react()],
 })
